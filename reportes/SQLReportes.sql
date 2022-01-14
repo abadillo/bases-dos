2874ea9a7aefedda19b074d28d367800a25bc99e
MJAJ <programadormjaj@gmail.com>
2022-01-13 23:12:20 -0400

    Creando archivo SQL de los reportes.

---
+++ reportes/SQLReportes.sql
@@ -0,0 +1,61 @@
+-- BALANCE GENERAL
+
+/*  Los jefes de las estaciones y los directores correspondientes monitorean el uso de este presupuesto a traves de
+    revisiones trimestrales y al final del año.
+    Cada estacion debe generar un informe con su balance:
+        Pago otorgado.
+        Fecha.
+        Hechos crudo recolectados.
+        Pieza de Inteligencia que lo utiliza (si aplica).
+        Valor de la pieza.
+        Ganancia o perdida alcanzada
+    Y los totales generales:
+        Total pagado.
+        Total obtenido por ventas de las piezas de inteligencia logradas por estos hechos aportados por informantes.
+*/
+
+
+
+
+-- FONDOS DE ALL Y APORTE DE ESTACION
+
+/*  El Director Ejecutivo debe poder consultar en cualquier momento de cuantos fondos dispone la All.
+    Asi como cada Jefe de Estacion debe poder saber a tiempo las ganancias netas anuales de cada estacion.
+*/
+
+
+
+
+-- INFORMACION DE SUS INFORMANTES
+
+/*  Se debe llevar un control sobre la eficacioa de estos tratos.
+    Saber el % de su eficacia, en proporcion de los hechos crudos aportados por tal informante y se han convertido
+    en piezas de inteligencia.
+    Los precios base para registro e informes de desempeño son en dolares.
+    Se debe saber la fecha de:
+        Obtencion del crudo.
+        Verificacion de dicho crudo.
+        Pago a informante.
+        Contruccion de la pieza.
+        Venta de la pieza.
+    Esta medida se genera semestralmente y se usa en combinacion con la productividad de los agentes.
+*/
+
+
+
+
+-- INTENTOS NO AUTORIZADOS
+
+/*  Caundo algun empleado trate de acceder a una pieza de inteligencia se debe quedar registrado y emitir una alerta
+    al jefe de la estacion.
+    Cada semana se debe emitir el reporte para el jefe su estacion.
+*/
+
+
+
+
+-- LISTA DE INFORMANTES
+
+/*  Es importante que otro empleado de All pueda acceder a la lista de informantes de un agente.    */
+
+
