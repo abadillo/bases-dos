678f7509299322ce4c5cb9611506b1208b83e3d9
Gabriel <alberto.manrique99rgmail.com>
2022-01-18 16:38:20 -0400

    trigger_cliente

---
+++ scripts/TRIGGER_INSERT_CLIENTE.sql
@@ -0,0 +1,63 @@
+--- FUNCION VALIDAR EL TIPO DE LUGAR ----
+CREATE OR REPLACE PROCEDURE VALIDAR_TIPO_LUGAR(id_lugar IN integer, tipo_va IN LUGAR.tipo%TYPE)
+AS $$
+DECLARE
+
+	lugar_reg LUGAR%ROWTYPE;
+	
+BEGIN
+
+	select * into lugar_reg from LUGAR where id = id_lugar AND tipo = tipo_va;
+	
+	IF (lugar_reg IS NULL) THEN
+		RAISE INFO 'El lugar/direccion ingresado debe ser de tipo %', tipo_va;
+		RAISE EXCEPTION 'El lugar/direccion ingresado debe ser de tipo %', tipo_va;
+	END IF;
+		
+END;
+$$ LANGUAGE plpgsql;
+
+----TRIGGER PARA VALIDAR EL INSERT DEL CLIENTE----
+
+CREATE OR REPLACE FUNCTION TRIGGER_CREAR_CLIENTE ()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS $$
+DECLARE
+	
+	lugar_reg lugar%rowtype;
+	
+BEGIN
+	---VALIDACIONES DE ATRIBUTOS---
+	---TIENE NOMBRE LA EMPRESA----
+	
+	IF (new.nombre_empresa IS NULL OR new.nombre_empresa = ' ') THEN
+		RAISE EXCEPTION 'EL NOMBRE DEL CLIENTE ESTA VACIO';
+	END IF;
+	
+	---TIENE PAGINA WEB LA EMPRESA---
+	
+	IF (new.pagina_web IS NULL OR new.pagina_web =' ') THEN
+		RAISE EXCEPTION 'DEBE INSERTAR UNA PAGINA WEB';
+	END IF;
+	
+	---VALIDA EL TIPO DE LUGAR (PAIS) DONDE SE REGISTRÓ EL CLIENTE---
+	call  VALIDAR_TIPO_LUGAR(new.fk_lugar_pais,'pais');
+	
+	RETURN new;
+
+END
+
+$$;
+
+--INSERT INTO cliente (nombre_empresa, pagina_web, exclusivo, telefonos, contactos, fk_lugar_pais) VALUES
+--('mexicaso', 'mexicaso.org.ve' ,true, ARRAY[CAST((58,4126909353) as telefono_ty), CAST((58,4165420879)as telefono_ty)],  ARRAY[ ROW('Eloisa', 'Petronila', 'Nolasco', 'White', '91 Sage Ave. Colorado Springs, CO 80911',  ROW(58,4121705701)), ROW('Nicolas', 'Abelardo', 'Tafoya', 'Peralta', '8370 Euclid Lane Harrisburg, PA 17109', ROW(58,4127728311))]::contacto_ty[], 4);
+
+
+DROP FUNCTION TRIGGER_CREAR_CLIENTE()
+
+DROP TRIGGER TRIGGER_CREAR_CLIENTE on cliente
+
+CREATE TRIGGER TRIGGER_CREAR_CLIENTE
+BEFORE INSERT on cliente
+FOR EACH ROW EXECUTE FUNCTION TRIGGER_CREAR_CLIENTE()
