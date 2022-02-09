aaeb5883f765d62dbae29e3c879e0f2064b6705a
abadillo <abadillo2924@gmail.com>
2022-02-09 02:22:24 -0400

    update desempeño

--- scripts_DW/SQL_AII_GRUPO09.sql
+++ scripts_DW/SQL_AII_GRUPO09.sql
@@ -9745,7 +9745,7 @@ CREATE TABLE T3_DESEMPEÑO_AII (
 
 
 
-CREATE OR REPLACE PROCEDURE TRANSFORMACION_T3_DESEMPENO_AII_DIMENSIONES_ANUAL (año IN varchar,semestre IN smallint)
+CREATE OR REPLACE PROCEDURE TRANSFORMACION_T3_DESEMPENO_AII_DIMENSIONES (año IN varchar, semestre IN integer)
 LANGUAGE PLPGSQL
 SECURITY DEFINER
 AS $$
@@ -9788,44 +9788,35 @@ BEGIN
    	RAISE NOTICE 'Filas insertadas en T3_REGION_OFICINA: %', n_filas_afect;
 
 	
-   	IF (semestre = 1) THEN
+   	IF (semestre = 0) THEN
    	
-   		INSERT INTO T3_TIEMPO (id_tiempo,semestre,año) VALUES ( to_date(año) . semestre, semestre, to_date(año) );
+   		INSERT INTO T3_TIEMPO (id_tiempo,semestre,año) VALUES ( (año::INTEGER)*10+semestre, null, to_date(año,'YYYY') );
    	
-   	ELSIF (semestre = 2) THEN
-   		
+   	ELSIF (semestre = 1 or semestre = 2) THEN
+   	
+   		INSERT INTO T3_TIEMPO (id_tiempo,semestre,año) VALUES ( (año::INTEGER)*10+semestre, semestre, to_date(año,'YYYY') );
+   	
    	ELSE 
-   		RAISE EXCEPTION 'Semestre solo puede ser 1 para el primer semestre y 2 para el segundo';
+   	
+   		RAISE EXCEPTION 'Semestre solo puede ser 1 para el primer semestre y 2 para el segundo. En el caso de las metricas anuales, semestre es igual a 0';
+   	
    	END IF;
    
    	
-   
-    INSERT INTO T3_TIEMPO (id_tiempo,semestre,año) VALUES (,null,);
-   
 END
 $$;
 
 
 
-CREATE TABLE T3_TIEMPO (
-
-    id_tiempo INTEGER NOT NULL,
-    año DATE,
-   	semestre SMALLINT
-    
-);
-
-
-
-
-
-CREATE OR REPLACE PROCEDURE TRANSFORMACION_T3_DESEMPENO_AII_ANUAL (año IN varchar,semestre IN smallint)
+CREATE OR REPLACE PROCEDURE TRANSFORMACION_T3_DESEMPENO_AII (año IN varchar, semestre IN integer)
 LANGUAGE PLPGSQL
 SECURITY DEFINER
 AS $$
 DECLARE 
     
 	n_filas_afect integer;
+	año_timestamp_ini timestamp;
+	año_timestamp_fin timestamp;
 	
 BEGIN
     
@@ -9834,15 +9825,393 @@ BEGIN
 	RAISE NOTICE '-----------------------------------------------------';
 	RAISE NOTICE ' ';
 	
+	año_timestamp_ini := to_timestamp(año, 'YYYY');	
 
+	RAISE NOTICE 'año metrica %', año_timestamp_ini;
 
+	IF (semestre = 0) THEN
+	
+		año_timestamp_fin := año_timestamp_ini + interval '1 year';
+		
+   	ELSIF (semestre = 1) THEN
+   		
+   		año_timestamp_fin := año_timestamp_ini + interval '6 months';
+   	
+   	ELSIF (semestre = 2) THEN
+   		
+   		año_timestamp_fin := año_timestamp_ini + interval '1 year';
+   		año_timestamp_ini := año_timestamp_ini + interval '6 months';
+   	
+   	ELSE 
+   	
+   		RAISE EXCEPTION 'Semestre solo puede ser 1 para el primer semestre y 2 para el segundo. En el caso de las metricas anuales, semestre es igual a 0';
+   	
+   	END IF;
+   	
+		
+	-- metrica cliente mas activo
 
+	INSERT INTO T3_DESEMPEÑO_AII (id_region_oficina, id_cliente, clienteMasActivo_año, numeroComprasCliente_año, id_tiempo) 
+  
+		SELECT DISTINCT ON (c.fk_region_oficina) c.fk_region_oficina, c.id, c.nombre_empresa, count(a.id) as numero_compras, (año::INTEGER)*10+semestre from t2_cliente c, t2_adquisicion a 
+		WHERE c.id = a.fk_cliente 
+		AND a.fecha_hora_venta BETWEEN año_timestamp_ini AND año_timestamp_fin
+		GROUP BY c.id, c.nombre_empresa 
+		ORDER BY c.fk_region_oficina ASC, numero_compras DESC;
 
+    GET DIAGNOSTICS n_filas_afect = ROW_COUNT;
 
+   	RAISE NOTICE 'Filas insertadas en T3_DESEMPEÑO_AII: %', n_filas_afect;
+   
+   	IF (n_filas_afect > 0) THEN 
+   
+   		CALL transformacion_t3_desempeno_aii_dimensiones(año,semestre);
+   	
+   	END IF;
 
 
 END
 $$;
+
+
+
+
+
+
+
+
+
+
+
+
+
+----------///////////- ------------------------------------------------------------------------------------ ///////////----------
+----------//////////- 			    Create tablas T1 - METRICA 1 y 2  	-- EJECUTAR COMO DEV 		       -//////////----------
+----------///////////- ----------------------------------------------------------------------------------- ///////////----------
+
+DROP TABLE IF EXISTS T1_INFORMANTE CASCADE;
+DROP TABLE IF EXISTS T1_PERSONAL_INTELIGENCIA CASCADE;
+DROP TABLE IF EXISTS T1_CRUDO_PIEZA CASCADE;
+DROP TABLE IF EXISTS T1_CRUDO CASCADE;
+DROP TABLE IF EXISTS T1_ANALISTA_CRUDO CASCADE;
+DROP TABLE IF EXISTS T1_HIST_CARGO CASCADE;
+
+
+
+CREATE TABLE T1_PERSONAL_INTELIGENCIA( -- 
+
+id INTEGER NOT NULL,
+
+    primer_nombre varchar(50) NOT NULL,
+    segundo_nombre varchar(50),
+    primer_apellido varchar(50) NOT NULL,
+    segundo_apellido varchar(50) NOT NULL,
+
+--    --foreign keys 
+    fk_lugar_ciudad integer NOT NULL,
+
+    CONSTRAINT T1_PERSONAL_INTELIGENCIA_PK PRIMARY KEY (id),
+
+    CONSTRAINT T1_PERSONAL_INTELIGENCIA_LUGAR_FK FOREIGN KEY (fk_lugar_ciudad) REFERENCES T1_LUGAR (id)
+    
+);
+
+
+CREATE TABLE T1_HIST_CARGO (
+    
+    fecha_inicio timestamp NOT NULL,
+    cargo varchar(20) NOT NULL, -- 'analista agente'
+    fk_personal_inteligencia integer NOT NULL,
+    fk_estacion integer NOT NULL,
+    fk_oficina_principal integer NOT NULL,
+
+    --CONSTRAINT T1_HIST_CARGO_PK PRIMARY KEY (fecha_inicio, fk_personal_inteligencia, fk_estacion, fk_oficina_principal),
+
+    CONSTRAINT T1_HIST_CARGO_PERSONAL_INTELIGENCIA_FK FOREIGN KEY (fk_personal_inteligencia) REFERENCES T1_PERSONAL_INTELIGENCIA (id),
+
+    CONSTRAINT T1_HIST_CARGO_CH_cargo CHECK ( cargo IN ('analista', 'agente') )    
+);
+
+
+CREATE TABLE T1_INFORMANTE ( -- 
+    
+     id integer NOT NULL,
+
+    nombre_clave varchar(100),
+    
+
+    -- agente de campo encargado del informante
+    fk_personal_inteligencia_encargado integer NOT NULL,    
+    fk_fecha_inicio_encargado timestamp NOT NULL,
+    fk_estacion_encargado integer NOT NULL,
+    fk_oficina_principal_encargado integer NOT NULL,
+    
+    CONSTRAINT T1_INFORMANTE_PK PRIMARY KEY (id)
+
+
+    -- CONSTRAINT T1_INFORMANTE_HIST_CARGO_encargado FOREIGN KEY (fk_fecha_inicio_encargado, fk_personal_inteligencia_encargado, fk_estacion_encargado, fk_oficina_principal_encargado) 
+    --     REFERENCES T1_HIST_CARGO (fecha_inicio, fk_personal_inteligencia, fk_estacion, fk_oficina_principal)
+
+);
+
+
+CREATE TABLE T1_CRUDO_PIEZA( -- 
+    
+    fk_pieza_inteligencia integer NOT NULL,
+    fk_crudo integer NOT NULL,
+
+    CONSTRAINT T1_CRUDO_PIEZA_ALT_PK PRIMARY KEY (fk_pieza_inteligencia, fk_crudo)
+
+);
+
+
+CREATE TABLE T1_CRUDO ( -- 
+
+ id integer NOT NULL,
+
+    fk_informante integer,
+
+    --estacion a donde pertence
+    fk_oficina_principal_pertenece integer NOT NULL,
+
+    --agente encargado
+    fk_estacion_agente integer NOT NULL,
+    fk_oficina_principal_agente integer NOT NULL,
+    fk_fecha_inicio_agente timestamp NOT NULL,
+    fk_personal_inteligencia_agente integer NOT NULL,
+
+    CONSTRAINT T1_CRUDO_PK PRIMARY KEY (id),
+
+    -- CONSTRAINT T1_CRUDO_HIST_CARGO_FK FOREIGN KEY (fk_fecha_inicio_agente, fk_personal_inteligencia_agente, fk_estacion_agente, fk_oficina_principal_agente) REFERENCES T1_HIST_CARGO (fecha_inicio, fk_personal_inteligencia, fk_estacion, fk_oficina_principal),
+
+    CONSTRAINT T1_CRUDO_INFORMANTE_FK FOREIGN KEY (fk_informante) REFERENCES T1_INFORMANTE (id)
+
+);
+
+
+CREATE TABLE T1_ANALISTA_CRUDO (
+
+
+    fecha_hora timestamp NOT NULL,
+    fk_crudo integer NOT NULL,
+
+    -- fks de hist_cargo
+    fk_fecha_inicio_analista timestamp NOT NULL,
+    fk_personal_inteligencia_analista integer NOT NULL,
+    fk_estacion_analista integer NOT NULL,
+    fk_oficina_principal_analista integer NOT NULL ,
+
+    CONSTRAINT T1_ANALISTA_CRUDO_PK PRIMARY KEY (fk_crudo, fk_personal_inteligencia_analista, fk_estacion_analista, fk_oficina_principal_analista),
+
+    CONSTRAINT T1_ANALISTA_CRUDO_CRUDO_FK FOREIGN KEY (fk_crudo) REFERENCES T1_CRUDO (id)
+    -- CONSTRAINT T1_ANALISTA_CRUDO_HIST_CARGO_FK FOREIGN KEY (fk_fecha_inicio_analista, fk_personal_inteligencia_analista, fk_estacion_analista, fk_oficina_principal_analista) REFERENCES T1_HIST_CARGO (fecha_inicio, fk_personal_inteligencia, fk_estacion, fk_oficina_principal)
+
+
+);
+
+
+
+----------///////////- ------------------------------------------------------------------------------------ ///////////----------
+----------//////////- 			    Create tablas T2 - METRICA 1 y 2  	-- EJECUTAR COMO DEV 		       -//////////----------
+----------///////////- ----------------------------------------------------------------------------------- ///////////----------
+
+DROP TABLE IF EXISTS T2_PAIS CASCADE;
+
+CREATE TABLE T2_PAIS (
+    id integer NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    fechac TIMESTAMP NOT NULL,
+    
+    CONSTRAINT T2_LUGAR_PK PRIMARY KEY (id)
+);
+
+
+
+----------///////////- ------------------------------------------------------------------------------------ ///////////----------
+----------//////////- 	 Create tablas T3 - METRICA 1  PRODUCTIVIDAD_EFICACIA 	-- EJECUTAR COMO DEV 		       -//////////----------
+----------///////////- ----------------------------------------------------------------------------------- ///////////----------
+
+
+DROP TABLE IF EXISTS T3_PERSONAL_INTELIGENCIA_LOOKUP CASCADE;
+DROP TABLE IF EXISTS T3_INFORMANTE CASCADE;
+DROP TABLE IF EXISTS T3_PAIS CASCADE;
+DROP TABLE IF EXISTS T3_TIEMPO CASCADE;
+DROP TABLE IF EXISTS T3_PRODUCTIVIDAD_EFICACIA CASCADE;
+
+CREATE TABLE T3_PERSONAL_INTELIGENCIA_LOOKUP (
+	
+	id_personal_inteligencia INTEGER,
+	nombre VARCHAR(100),
+	fechac TIMESTAMP
+	
+);
+
+CREATE TABLE T3_INFORMANTE (
+
+	id_informante INTEGER,
+	nombre_clave VARCHAR(50),
+	id_personal_inteligencia INTEGER,
+	fechac TIMESTAMP,
+	fk_personal INTEGER
+		
+);
+
+
+-- CREATE TABLE T3_TIEMPO (
+
+--     id_tiempo INTEGER NOT NULL,
+--     semestre SMALLINT,
+--     año DATE   
+
+-- );
+
+CREATE TABLE T3_PAIS (
+
+	id_pais INTEGER,
+	nombre VARCHAR(50),
+	fechac timestamp	
+);
+
+CREATE TABLE T3_PRODUCTIVIDAD_EFICACIA (
+    id_tiempo integer NOT NULL,
+    id_lugar integer,
+    id_informante integer,
+    id_oficina integer,
+    id_personal integer,
+    %EficaciaInformante numeric (6,3),
+    %ProdPromedioAgentesPais numeric (6,3),
+    %ProdPromedioAnalistasPais numeric (6,3),
+    %ProdPromedioAgentesOficina numeric (6,3),
+    %ProdPromedioAnalistasOficina numeric (6,3),
+    %ProdGeneralAgente ProdEmpleado,
+    %ProdGeneralAnalista ProdEmpleado
+)
+
+
+---
+---
+---
+---
+---
+---
+---
+---
+---
+----------///////////- ------------------------------------------------------------------------------------ ///////////----------
+----------//////////- 		    EXTRACCION DE DATOS  - METRICA 1 y 2  	-- EJECUTAR COMO DEV 		     -//////////----------
+----------///////////- ----------------------------------------------------------------------------------- ///////////----------
+
+
+CREATE OR REPLACE PROCEDURE EXTRACCION_A_T1_PRODUCTIVIDAD_EFICACIA ()
+LANGUAGE PLPGSQL
+SECURITY DEFINER
+AS $$
+DECLARE 
+    maxid INTEGER;
+	n_filas_afect integer;
+BEGIN
+        
+	RAISE NOTICE ' ';
+	RAISE NOTICE 'EXTRACCION_A_T1_PRODUCTIVIDAD_EFICACIA - %', NOW();
+	RAISE NOTICE '-----------------------------------------------------';
+	RAISE NOTICE ' ';
+	
+	-- T1_PERSONAL_INTELIGENCIA
+
+    SELECT COALESCE(max(id),0) INTO maxid FROM T1_PERSONAL_INTELIGENCIA;
+   
+    INSERT INTO T1_PERSONAL_INTELIGENCIA 
+        (id, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, fk_lugar_ciudad) 
+    SELECT id, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, fk_lugar_ciudad 
+    FROM PERSONAL_INTELIGENCIA c
+        WHERE c.id > maxid;
+    GET DIAGNOSTICS n_filas_afect = ROW_COUNT;
+
+	RAISE NOTICE 'Filas insertadas en T1_PERSONAL_INTELIGENCIA: %', n_filas_afect;
+	
+
+	-- T1_HIST_CARGO
+    /*
+    fecha_inicio, cargo, fk_personal_inteligencia, fk_estacion, fk_oficina_principal
+
+	SELECT COALESCE(max(id), 0) INTO maxid from T1_HIST_CARGO;
+    
+	INSERT INTO T1_HIST_CARGO (id, nombre, descripcion, topico) 
+    SELECT id, nombre, descripcion, topico FROM HIST_CARGO c
+        WHERE c.id > maxid;
+	GET DIAGNOSTICS n_filas_afect = ROW_COUNT;
+
+   	RAISE NOTICE 'Filas insertadas en T1_CLAS_TEMA: %', n_filas_afect;
+    */
+
+
+   	-- T1_INFORMANTE 
+    
+    SELECT COALESCE(max(id),0) INTO maxid FROM T1_INFORMANTE;
+
+    INSERT INTO T1_INFORMANTE (id, nombre_clave, fk_personal_inteligencia_encargado, fk_fecha_inicio_encargado, fk_estacion_encargado, fk_oficina_principal_encargado) 
+    SELECT id, nombre_clave, fk_personal_inteligencia_encargado, fk_fecha_inicio_encargado, fk_estacion_encargado, fk_oficina_principal_encargado 
+    FROM INFORMANTE c
+        WHERE c.id > maxid;
+    GET DIAGNOSTICS n_filas_afect = ROW_COUNT;
+
+	RAISE NOTICE 'Filas insertadas en T1_INFORMANTE: %', n_filas_afect;
+
+    INSERT INTO T1_INFORMANTE (id, fk_personal_inteligencia_encargado, fk_fecha_inicio_encargado, fk_estacion_encargado, fk_oficina_principal_encargado) 
+    SELECT id, fk_personal_inteligencia_encargado, fk_fecha_inicio_encargado, fk_estacion_encargado, fk_oficina_principal_encargado 
+    FROM INFORMANTE_ALT c
+        WHERE c.id > maxid;
+    GET DIAGNOSTICS n_filas_afect = ROW_COUNT;
+
+	RAISE NOTICE 'Filas insertadas en T1_INFORMANTE_ALT: %', n_filas_afect;
+    
+    -- T1_CRUDO_PIEZA 
+    /*
+    fk_pieza_inteligencia, fk_crudo
+    SELECT COALESCE(max(id),0) INTO maxid FROM T1_CRUDO_PIEZA;
+    INSERT INTO T1_OFICINA_PRINCIPAL (id, nombre, fk_lugar_ciudad) 
+    SELECT id, nombre, fk_lugar_ciudad FROM T1_CRUDO_PIEZA c
+        WHERE c.id > maxid;
+    GET DIAGNOSTICS n_filas_afect = ROW_COUNT;
+
+	RAISE NOTICE 'Filas insertadas en T1_CRUDO_PIEZA: %', n_filas_afect;
+    */
+
+	-- T1_CRUDO  
+
+
+    SELECT COALESCE(max(id),0) INTO maxid FROM T1_CRUDO;
+    
+    INSERT INTO T1_CRUDO (id, fk_informante, fk_oficina_principal_pertenece, fk_estacion_agente, fk_oficina_principal_agente, fk_fecha_inicio_agente, fk_personal_inteligencia_agente)
+    SELECT id, fk_informante, fk_oficina_principal_pertenece, fk_estacion_agente, fk_oficina_principal_agente, fk_fecha_inicio_agente, fk_personal_inteligencia_agente 
+    FROM CRUDO c
+        WHERE c.id > maxid;
+	GET DIAGNOSTICS n_filas_afect = ROW_COUNT;
+    
+	RAISE NOTICE 'Filas insertadas en T1_CRUDO: %', n_filas_afect;
+
+    INSERT INTO T1_CRUDO (id, fk_informante, fk_oficina_principal_pertenece, fk_estacion_agente, fk_oficina_principal_agente, fk_fecha_inicio_agente, fk_personal_inteligencia_agente) 
+    SELECT id, fk_informante, fk_oficina_principal_pertenece, fk_estacion_agente, fk_oficina_principal_agente, fk_fecha_inicio_agente, fk_personal_inteligencia_agente 
+    FROM CRUDO_ALT c
+        WHERE c.id > maxid;
+    GET DIAGNOSTICS n_filas_afect = ROW_COUNT;
+
+	RAISE NOTICE 'Filas insertadas de CRUDO_ALT: %', n_filas_afect;
+
+
+END
+$$;
+
+
+
+
+
+
+
+
+
+
+
 
 
 
